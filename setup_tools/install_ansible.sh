20a22bfb935e7ee9b7d0e21cefdaa7fdca87f094
Adam King <rak@us.ibm.com>
2017-02-06 10:50:36 -0500

    Fix issues with setup_tools
    Move playbook to the root level.
    Make install_ansible.sh executable
    Stop installing jq
    
    Signed-off-by: Adam King <rak@us.ibm.com>

--- setup_tools/install_ansible.sh
+++ setup_tools/install_ansible.sh
@@ -2,8 +2,8 @@
 python --version
 echo "apt-get -qq update"
 sudo apt-get -qq update
-echo "apt-get -qq -y install python-dev libssl-dev libffi-dev jq"
-sudo apt-get -qq -y install python-dev libssl-dev libffi-dev jq
+echo "apt-get -qq -y install python-dev libssl-dev libffi-dev"
+sudo apt-get -qq -y install python-dev libssl-dev libffi-dev
 echo "pip install pycrypto pyyaml ansible --quiet"
 sudo pip install pycrypto pyyaml ansible --quiet
 
