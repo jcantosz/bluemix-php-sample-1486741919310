0691e9346d42abdf9bb1a7f2e07928b5c8a27ffe
Jesse Antoszyk <jcantosz@users.noreply.github.com>
2017-02-10 13:25:22 -0500

    Update install_ansible.sh

--- setup_tools/install_ansible.sh
+++ setup_tools/install_ansible.sh
@@ -4,8 +4,8 @@ echo "apt-get -qq update"
 sudo apt-get -qq update
 echo "apt-get -qq -y install python-dev libssl-dev libffi-dev"
 sudo apt-get -qq -y install python-dev libssl-dev libffi-dev
-echo "pip install pycrypto pyyaml ansible --quiet"
-sudo pip install pycrypto pyyaml ansible --quiet
+echo "pip install pycrypto pyyaml boto ansible --quiet"
+sudo pip install pycrypto pyyaml boto ansible --quiet
 
 echo "apt-get -qq clean"
 sudo apt-get -qq clean
